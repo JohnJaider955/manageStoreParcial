e15a3642204227f8c1d263f2d450ee2bb6343af7
JohnJaider955 <127434052+JohnJaider955@users.noreply.github.com>
2023-03-13 14:28:20 -0500

    Add files via upload

---
+++ manage_store-main/src/main/java/co/edu/umanizales/manage_store/model/Store.java
@@ -0,0 +1,10 @@
+package co.edu.umanizales.manage_store.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class Store {
+    private String code;
+    private String name;
+}
